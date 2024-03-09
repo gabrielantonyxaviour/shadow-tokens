62e34a834395c8b54a3988b7cb7889d826b7fe99
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-09 17:13:01 -0500

    remove comments at top

--- blockend/evm/contracts/ShadowProtocol.sol
+++ blockend/evm/contracts/ShadowProtocol.sol
@@ -2,16 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-
-// Functionalities
-
-// 1. User locks his NFT in this vault. Which mints the NFT in the secret network. where he can own, lend or fractionalize it privately.
-// 2. User can make a transaction to retreive the NFT back to EVM network if he owns the NFT completely or can own a fraction.
-
-// 1. lockNFT
-// 2. retreiveNFT
-// 3. retreiveNFTFraction
-
 import "./interface/IShadows.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Create2.sol";
