169e765136e7f03d3831911d3ca58f3a3c23d70c
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-03-11 08:43:06 +0000

    add storage vars

--- blockend/secret/src/state.rs
+++ blockend/secret/src/state.rs
@@ -7,7 +7,7 @@ pub struct MyMessage {
     pub sender: String,
     pub message: String,
 }
-
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
 pub struct Asset {
     pub token_address: String,
     pub token_id: u64,
@@ -17,6 +17,11 @@ pub struct Asset {
     pub owner_pub_key: PublicKey,
 }
 
+pub struct Assets {
+    pub assets: Vec<Asset>
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
 pub struct Listing {
     pub asset_id: String,
     pub fractions: u128,
@@ -24,4 +29,13 @@ pub struct Listing {
     pub active_time: u64
 }
 
+pub struct Listings {
+    pub listings: Vec<Listing>
+}
+
 pub const STORED_MESSAGE: Item<MyMessage> = Item::new(b"stored_message");
+pub const LISTING_COUNT: Item<u128> = Item::new(b"listing_count");
+pub const ASSET_COUNT: Item<u128> = Item::new(b"asset_count");
+pub const LISTINGS: Item<Listings> = Item::new(b"listings");
+pub const ASSETS: Item<Assets> = Item::new(b"assets");
+
